f564d52c7753d43b688cb8014191508f135cb90e
raflima1981 <raflima@usp.br>
2021-10-26 10:10:48 +0200

    New files and text (main text and SOM) related to the 2nd manuscript draft send to all co-authors

---
+++ R/99_map_sis_connect_csv.R
@@ -0,0 +1,25 @@
+################################################################
+#### WHERE EACH IUCN SIS CONNECT FILES ARE BEING GENERATED? ####
+################################################################
+
+## CODES DONE, FILES IN FOLDER
+# "commonnames_threat.csv" => 02_prep_species_info.R - Appendix ZZ
+# "countries_threat.csv" => 02_prep_species_info.R - Appendix 
+# "credits_threat.csv" => 02_prep_species_info.R - Appendix         
+# "endemism_threat.rds" => 02_prep_species_info.R - Appendix 
+# "habitats_threat.csv" => 02_prep_species_info.R - Appendix 
+# "plantspecific_threat.csv" => 02_prep_species_info.R - Appendix CC
+# "prev_assessments_threat.rds" => 02_prep_species_info.R - Appendix 
+# "synonyms_threat.csv" => 02_prep_species_info.R - Appendix YY
+# "taxonomy_threat.csv" => 02_prep_species_info.R - Appendix XX
+# "threats_threat.csv" => 02_prep_species_info.R - Appendix 
+# "usetrade_threat.csv" => 02_prep_species_info.R - Appendix 
+
+
+## CODES PENDING
+# "conservationneeded_threat.csv" => 02_prep_species_info.R
+# "researchneeded_threat.csv" => 02_prep_species_info.R
+# "references_threat.csv" => 02_prep_species_info.R
+
+# "allfields_threat.csv" =>
+# "assessments_threat.csv" =>
